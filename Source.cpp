2ed4545542894c6cfa67d3dc244c06267dc4cb23
zxctatar <maksimknutov80@gmail.com>
2023-11-08 17:30:35 +0300

    fix

--- Source.cpp
+++ Source.cpp
@@ -9,48 +9,39 @@ int main() {
 	std::cout << "Введите текст для шифрования" << '\n';
 	std::getline(std::cin, text);
 
-	int i{};
-	bool b = false;
-
-	while (i < text.size()) {
+	for (int i = 0; i < text.size(); ) {
 		int count{};
+
 		for (; isalpha(int(text[i]));) {
 			count++;
 			i++;
 		}
 
 		for (int q = i - count; q < i; ++q) {
-			if (text[q] + count < 'A' || text[q] + count > 'Z' || text[q] + count < 'a' || text[q] + count > 'z') {
-				char temp = text[q];
-				for (int w = i - count; w < i; ++w) {
-					if (!isalpha(int(temp + 1))) {
-						if (temp == 'Z') {
-							temp = 'A';
-						}
-						else {
-							temp = 'a';
-						}
-						continue;
+			char temp = text[q];
+			for (int w = i - count; w < i; ++w) {
+				if (!isalpha(int(temp + 1))) {
+					if (temp == 'Z') {
+						temp = 'A';
 					}
-					temp += 1;
+					else {
+						temp = 'a';
+					}
+					continue;
 				}
-				cipher += temp;
-				continue;
+				temp += 1;
 			}
-			cipher += text[q] + count;
+			cipher += temp;
+			continue;
+
 		}
 
 		for (; !isalpha(int(text[i])); ) {
 			if (text[i] == '\0') {
-				b = true;
 				break;
 			}
 			cipher += text[i];
 			i++;
-		}
-
-		if (b) {
-			break;
 		}
 	}
 
